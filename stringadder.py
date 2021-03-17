0d22ddc8484c63828ceb44723416ab3b282ada98
Garrett Aspacher <gma615@gmail.com>
2021-03-17 13:13:00 -0600

    fixed code a bit

--- stringadder.py
+++ stringadder.py
@@ -1,28 +1,29 @@
 import re
 # this function takes in a string with no number and appends 1 automatically. If the number provided is greater than 1, it adds 1 to the number provided.
-def StringAdder():
-    text = input("Please ender a word with or without a positive number after\n")
+def StringAdder(string):
+    # empty string variable, used for joining a list and converting
     str1 = ""
     # finds the number in the string provided by user
     result = re.findall(r'\d+',text)
-    # joins list to make an empty string
+    # joins list to make a string so that we can see how long the string is
     result1 = str1.join(result)
     # creates an integer of the length of the string number provided
     result2 = len(result1)
-    # if the result is empty list
+    # if the result of the integer search is empty list or nothing
     if result == [] :
         text1 = text + "1"
         print(text1)
-        exit()
-    text1 = text[:-result2]
-    num = int(str1.join(result))
-    num += 1        
-    num1 = str(num)
-    string = text1 + num1
-    print(string)
+    # otherwise, if its any integer of any length, this does what is needed
+    else:
+        text1 = text[:-result2]
+        num = int(str1.join(result))
+        num += 1        
+        num1 = str(num)
+        string = text1 + num1
+        print(string)
 
 
 
-    
-StringAdder()
-exit()
+text = input("Please ender a word with or without a positive number after\n") 
+StringAdder(text)
+
