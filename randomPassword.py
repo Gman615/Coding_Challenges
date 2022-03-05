afcc30cca5367f0a91b428998f1af713e3d42518
Garrett Aspacher <gma615@gmail.com>
2022-03-05 10:44:49 -0500

    updating further

--- randomPassword.py
+++ randomPassword.py
@@ -1,15 +1,17 @@
 import random
 
-def randomPW():
+
+# takes user input on length of password
+def randomPW(length):
     randchar = 'a A b B c C d D e E f F g G h H i I j J k K l L m M n N o O p P q Q r R s S t T u U v V w W x X y Y z Z 1 2 3 4 5 6 7 8 9 0 ~ ` ! @ # $ % ^ & * _ - + = < > ?'.split()
     # below line of code grabs a random sample from the string above which is 12 digits long
-    randchar1 = random.sample(randchar, 12)
+    randchar = random.sample(randchar, length)
     # removes the spaces and brackets form the generated code
-    randchar2 = ''.join(randchar1)
-    print(randchar2)
+    randchar = ''.join(randchar)
+    print(randchar)
 
 
 
 
-
-randomPW()
+passwordLength = int(input("How long would you like your password to be?\n"))
+randomPW(passwordLength)
